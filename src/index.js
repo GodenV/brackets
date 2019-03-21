82428ea9f6cf67aed9d175b884082b1fc3626601
Vadzim Kersnovskiy <kersnachv@mail.ru>
2019-03-22 02:12:59 +0300

    Ready task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var mas = [];
+  for(var i = 0; i<str.length; i++)
+    if(findin(str[i], bracketsConfig, 0) == -1 ){
+        if(bracketsConfig[findin(str[i],bracketsConfig,1)][0] !=  mas.pop())
+          return false;
+      }
+    else
+      if(bracketsConfig[findin(str[i], bracketsConfig, 0)] == bracketsConfig[findin(str[i], bracketsConfig, 1)] && mas[mas.length - 1] == bracketsConfig[findin(str[i], bracketsConfig, 0)][0])
+        mas.pop();
+      else
+        mas.push(str[i]);
+  if(mas.length == 0)
+    return true;
+    return false;
+}
+
+function findin(s, bracckets, n){
+  for(var j = 0; j <bracckets.length; j++)
+    if(bracckets[j][n] == s)
+      return j;
+  return -1;
 }
